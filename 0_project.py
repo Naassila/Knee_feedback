928c108b7b100d884a9046996a2246cd06431937
Nassila <assila.najoua@gmail.com>
2023-04-03 14:23:45 +0200

    Set up of the project to run the pipeline

---
+++ 0_project.py
@@ -0,0 +1,61 @@
+"""""
+ Create project
+"""""
+
+import shutil
+from project_paths import *
+from pyosim import Conf, Project
+
+Initialize = False
+change_marker_names = False
+# ACTUAL PARTICIPANT TO PROCESS #
+participant_to_do = -1
+
+project = Project(path = PROJECT_PATH)
+
+if Initialize:
+    project.create_project()
+
+
+shutil.copy(CONF_TEMPLATE, PROJECT_PATH)
+project.update_participants()
+conf = Conf(project_path=PROJECT_PATH)
+
+conf.check_confs()
+
+participants = conf.get_participants_to_process()
+d = {}
+for iparticipant in participants:
+    pseudo_in_path = (
+        iparticipant[0].upper() + iparticipant[1:-1] + iparticipant[-1].upper()
+    )
+
+    trials = (
+        f"{RAW_PATH}/{iparticipant}"
+    )
+
+    d.update(
+        {
+            iparticipant: {
+                "markers": {"data": [trials]},
+            }
+        }
+    )
+conf.add_conf_field(d)
+
+# assign channel fields to targets fields
+for ikind, itarget in targets.items():
+    for iparticipant in participants:
+        print(f"\t{iparticipant} - {ikind}")
+        if "assigned" not in conf.get_conf_field(iparticipant, [ikind]):
+            if change_marker_names:
+                fields = FieldsAssignment(
+                    directory=conf.get_conf_field(iparticipant, field=[ikind, "data"]),
+                    targets=itarget,
+                    kind=ikind,
+                )
+                fields_conf = {'assigned' : fields.output}
+            else :
+                fields_conf =  {'assigned' : itarget}
+            print("\t\tdone")
+            conf.add_conf_field({iparticipant: fields_conf})
