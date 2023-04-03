3540c1211f028ce294d106576566d28651ecb360
Nassila <assila.najoua@gmail.com>
2023-04-03 14:24:40 +0200

    Details of the project
    Paths of the raw and analysis folders, as well as the marker names

---
+++ project_paths.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+PROJECT_PATH = Path("D:/Knee_Flexion_auditory_tactile_feedback/Analysis/results")
+RAW_PATH = Path("D:/Knee_Flexion_auditory_tactile_feedback/Raw_data/Young")
+CONF_TEMPLATE = Path("D:/Knee_Flexion_auditory_tactile_feedback/Raw_data/_conf.csv")
+
+targets = {
+    "markers": [
+        "L.PSIS", "L.ASIS", "R.ASIS", "R.PSIS",
+        "L.Thigh.Upper", "L.Thigh.Front", "L.Thigh.Rear", "L.Thigh",
+        "L.Knee.Lat", "L.Knee.Med",
+        "L.Shank.Upper", "L.Shank.Front", "L.Shank.Rear", "L.Shank",
+        "L.Ankle.Lat", "L.Ankle.Med",
+        "P", "T", "F",
+        "L.Heel", "L.Midfoot.Sup", "L.Midfoot.Lat", "L.Toe.Lat", "L.Toe.Med", "L.Toe.Tip"]
+}
